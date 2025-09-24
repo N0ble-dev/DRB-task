1f6bf2f77bcd23868a4f87634b3a57ee9be0848f
Nabil Alex <nabilmohaed46@gmail.com>
2025-09-24 10:58:14 +0300

    add providers

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,9 @@
-// app/layout.tsx
-
 import type { Metadata } from "next";
 import "./globals.css";
-// Make sure to import SidebarTrigger
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import AppSidebar from "@/components/APPSidebar";
+import Link from "next/link";
+import { RoutesProvider } from "@/context/RoutesContext";
 
 export const metadata: Metadata = {
   title: "DRB Task",
@@ -19,16 +18,22 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="flex h-screen bg-muted/40">
-        <SidebarProvider>
-          <AppSidebar />
-          <div className="flex flex-1 flex-col">
-            <header className=" sticky top-0 z-10 flex h-14 items-center gap-4 border-b bg-background px-4 sm:h-16 sm:px-6">
-              <SidebarTrigger className="cursor-pointer w-12 h-12" />
-              <h1 className="text-lg font-semibold ">Dashboard</h1>
-            </header>
-            <main className="flex-1 overflow-auto p-4 sm:p-6">{children}</main>
-          </div>
-        </SidebarProvider>
+        <RoutesProvider>
+          <SidebarProvider>
+            <AppSidebar />
+            <div className="flex flex-1 flex-col">
+              <header className=" sticky top-0 z-10 flex h-14 items-center gap-4 border-b bg-background px-4 sm:h-16 sm:px-6">
+                <SidebarTrigger className="cursor-pointer w-12 h-12" />
+                <Link href="/" className="text-lg font-semibold ">
+                  Dashboard
+                </Link>
+              </header>
+              <main className="flex-1 overflow-auto p-4 sm:p-6">
+                {children}
+              </main>
+            </div>
+          </SidebarProvider>
+        </RoutesProvider>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,14 @@
 "use client";
 import RenderDriversTable from "@/components/tables/drivers/page";
-import { Driver, Route } from "@/interfaces/index";
+import { Driver } from "@/interfaces/index";
 import driversData from "@/data/drivers.json";
-import routesData from "@/data/routes.json";
 import RenderRoutesTable from "@/components/tables/routes/page";
 
 export default function Home() {
   return (
     <>
       <RenderDriversTable data={driversData as Driver[]} />
-      <RenderRoutesTable data={routesData as Route[]} />
+      <RenderRoutesTable />
     </>
   );
 }
